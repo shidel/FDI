406bd181b76cbf8dce0e355dd5a9ec34477b18c8
Shidel <shidel@users.noreply.github.com>
2016-01-29 07:07:20 -0500

    Build process improvements

--- commit.sh
+++ commit.sh
@@ -6,7 +6,6 @@ git add PACKAGES\*.TXT
 git add V8POWER\*.TXT
 git add WELCOME\*
 git add *.sh
-git add $(find -P BUILD -type f)
 git add $(find -P SETTINGS -type f)
 git add $(find -P FDISETUP -type f)
 git add $(find -P LANGUAGE -type f -iname *.def)
