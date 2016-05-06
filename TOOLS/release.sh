69ac62bbc9ededcafc9eed8ec3b581a39b9f0ca3
Shidel <shidel@users.noreply.github.com>
2016-05-06 14:04:32 -0400

    TOOL improvements

--- TOOLS/release.sh
+++ TOOLS/release.sh
@@ -2,8 +2,94 @@
 
 
 PRERELEASE='.pre'
-
 DESTINATION='Downloads'
+
+SOURCE=
+FLOPPY=FLOPPY.img
+ELTORITO=FDI.img
+SUSB=FDI-SLIM.img
+BUSB=FDI-USB.img
+
+check_dir () {
+
+    [[ ! -d "$1" ]] && mkdir "$1"
+    [[ ! -d "$1" ]] && {
+        echo $1 build directory missing
+        exit 1
+    }
+}
+
+load_settings () {
+
+    local BASE="${0##*/}"
+    local BASE="${BASE%.*}"
+
+    [[ -f "${PWD}/${BASE}.cfg" ]] && {
+        CFG="${PWD}/${BASE}.cfg"
+        . "${CFG}"
+    }
+
+    local T="${HOME}/${BASE}.cfg"
+
+    [[ -f "${T}" ]] && {
+        CFG="${T}"
+        . "${CFG}"
+    }
+
+    [[ "${CFG}" == "" ]] && {
+        CFG="${PWD}/${BASE}.cfg"
+        echo >"${CFG}"
+    }
+
+}
+
+save_settings () {
+
+    echo "#!/bin/sh" >"${CFG}"
+    echo >>"${CFG}"
+    echo "SOURCE=\"${SOURCE}\"">>"${CFG}"
+    echo "PRERELEASE=\"${PRERELEASE}\"">>"${CFG}"
+    echo "DESTINATION=\"${DESTINATION}\"">>"${CFG}"
+    echo "FLOPPY=\"${FLOPPY}\"">>"${CFG}"
+    echo "ELTORITO=\"${ELTORITO}\"">>"${CFG}"
+    echo "SUSB=\"${SUSB}\"">>"${CFG}"
+    echo "BUSB=\"${BUSB}\"">>"${CFG}"
+
+}
+
+check_settings () {
+
+    while [[ ! -d "${SOURCE}" ]]; do
+        echo Unable to locate virtual machine files. Where is it?
+        read -e SOURCE
+        [[ -d "${SOURCE}" ]] && save_settings
+    done;
+
+    while [[ ! -f "${SOURCE}/${FLOPPY}" ]]; do
+        echo Unable to locate normal floppy image. Where is it?
+        read -e FLOPPY
+        [[ -f "${SOURCE}/${FLOPPY}" ]] && save_settings
+    done;
+    while [[ ! -f "${SOURCE}/${ELTORITO}" ]]; do
+        echo Unable to locate El Torito Floppy image. Where is it?
+        read -e ELTORITO
+        [[ -f "${SOURCE}/${ELTORITO}" ]] && save_settings
+    done;
+    while [[ ! -f "${SOURCE}/${SUSB}" ]]; do
+        echo Unable to locate SLIM USB image. Where is it?
+        read -e SUSB
+        [[ -f "${SOURCE}/${SUSB}" ]] && save_settings
+    done;
+    while [[ ! -f "${SOURCE}/${BUSB}" ]]; do
+        echo Unable to locate FULL USB image. Where is it?
+        read -e BUSB
+        [[ -f "${SOURCE}/${BUSB}" ]] && save_settings
+    done;
+
+}
+
+load_settings
+check_settings
 
 RELEASE=0
 PROJECT=$(echo ${PWD##*/})
@@ -14,12 +100,11 @@ TODAY=$(date +'%Y-%m-%d')
 
 [[ -d "${HOME}/${DESTINATION}/${PROJECT}" ]] && rm -rf "${HOME}/${DESTINATION}/${PROJECT}"
 
-# mkdir -p "${HOME}/${DESTINATION}/${PROJECT}"
-# cp -r * "${HOME}/${DESTINATION}/${PROJECT}/"
-# rm -f "${HOME}/${DESTINATION}/${PROJECT}/"*.sh
+rm "${HOME}/${DESTINATION}/${PROJECT}-USB"*.${FORMAT} >/dev/null
+rm "${HOME}/${DESTINATION}/${PROJECT}-SLIM"*.${FORMAT} >/dev/null
 
 mkdir -p "${HOME}/${DESTINATION}/${PROJECT}"
-cp -r "${HOME}/Documents/Virtual Machines.localized/FDI.img" "${HOME}/${DESTINATION}/${PROJECT}/"
+cp -r "${SOURCE}/${FLOPPY}" "${HOME}/${DESTINATION}/${PROJECT}/FDI.img"
 
 ARCHIVE="${PROJECT}-${TODAY}-${RELEASE}${PRERELEASE}.${FORMAT}"
 while [[ -f "${HOME}/${DESTINATION}/${ARCHIVE}" ]] ; do
@@ -39,18 +124,15 @@ if [[ -f 'README.md' ]] ; then
 	cp 'README.md' "${HOME}/${DESTINATION}/${PROJECT}/README.md"
 fi;
 
-rm "${HOME}/${DESTINATION}/${PROJECT}-USB."* >/dev/null
-rm "${HOME}/${DESTINATION}/${PROJECT}-SLIM."* >/dev/null
-
 CURDIR="$PWD"
 
 cd "${HOME}/${DESTINATION}"
-cp "${HOME}/Documents/Virtual Machines.localized/Platforms/Development/FDI Builder.vmwarevm/USB-512MB-flat.vmdk" "${HOME}/${DESTINATION}/${PROJECT}-USB.img"
-cat "${HOME}/Documents/Virtual Machines.localized/Platforms/Development/FDI Builder.vmwarevm/USB-512MB.vmdk" | sed 's/USB-512MB-flat.vmdk/FDI-USB.img/g' >"${HOME}/${DESTINATION}/${PROJECT}-USB.vmdk"
-zip -9 -r "${PROJECT}-USB.zip" "${PROJECT}-USB.img" "${PROJECT}-USB.vmdk"
-cp "${HOME}/Documents/Virtual Machines.localized/Platforms/Development/FDI Builder.vmwarevm/USB-32MB-flat.vmdk" "${HOME}/${DESTINATION}/${PROJECT}-SLIM.img"
-cat "${HOME}/Documents/Virtual Machines.localized/Platforms/Development/FDI Builder.vmwarevm/USB-32MB.vmdk" | sed 's/USB-32MB-flat.vmdk/FDI-SLIM.img/g' >"${HOME}/${DESTINATION}/${PROJECT}-SLIM.vmdk"
+cp "${SOURCE}/${SUSB}" "${HOME}/${DESTINATION}"
+cp "${SOURCE}/${SUSB%.*}.vmdk" "${HOME}/${DESTINATION}"
 zip -9 -r "${PROJECT}-SLIM.zip" "${PROJECT}-SLIM.img" "${PROJECT}-SLIM.vmdk"
+cp "${SOURCE}/${BUSB}" "${HOME}/${DESTINATION}"
+cp "${SOURCE}/${BUSB%.*}.vmdk" "${HOME}/${DESTINATION}"
+zip -9 -r "${PROJECT}-USB.zip" "${PROJECT}-USB.img" "${PROJECT}-USB.vmdk"
 
 rm "${HOME}/${DESTINATION}/${PROJECT}-"*.img >/dev/null
 rm "${HOME}/${DESTINATION}/${PROJECT}-"*.vmdk >/dev/null
