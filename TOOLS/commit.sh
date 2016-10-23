9649bba548fd0079e0101f53e02b45c33f847df8
Shidel <shidel@users.noreply.github.com>
2016-10-23 07:08:51 -0400

    Keyboard Layout Defaults

--- TOOLS/commit.sh
+++ TOOLS/commit.sh
@@ -8,8 +8,10 @@ git add WELCOME\*
 git add TOOLS\*.sh TOOLS\*.bat
 git add $(find -P SETTINGS -type f)
 git add $(find -P FDISETUP -type f)
-git add $(find -P LANGUAGE -type f -iname *.def)
+git add $(find -P KEYBOARD -type f)
+git add $(find -P LANGUAGE -type f -iname *.DEF)
 git add $(find -P LANGUAGE -type f -iname *.BAT)
+git add $(find -P LANGUAGE -type f -iname *.LST)
 
 git commit -m "$*"
 git push
